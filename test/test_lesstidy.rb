fff0adc1357a78528de4b71662ad0e5a7260a75f
Rico Sta. Cruz <rico@ambiescent.com>
2010-08-07 03:15:42 +0800

    Add support for + and = and quoted strings in selectors.

--- test/test_lesstidy.rb
+++ test/test_lesstidy.rb
@@ -1,4 +1,13 @@
 require 'helper'
 
 class TestLesstidy < Test::Unit::TestCase
+  should "Satisfy some simple tests" do
+    t("div { color: red; }")
+    t("div + div { color: red; }")
+  end
+
+private
+  def t(css)
+    Lesstidy::Document.new css
+  end
 end
