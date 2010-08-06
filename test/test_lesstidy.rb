ff125db05c56bc58363bc365ff39f15a603eddec
Rico Sta. Cruz <rico@ambiescent.com>
2010-08-07 03:31:31 +0800

    Add support for quoted text in values and mixin params.

--- test/test_lesstidy.rb
+++ test/test_lesstidy.rb
@@ -1,9 +1,26 @@
 require 'helper'
 
 class TestLesstidy < Test::Unit::TestCase
-  should "Satisfy some simple tests" do
-    t("div { color: red; }")
-    t("div + div { color: red; }")
+  pass_tests = {
+    pass1: "div { color: red; }",
+    pass2: "div + div { color: red; }",
+    pass3: "div[name=foo] { color: red; }",
+    pass4: "div[name='foo'] { color: red; }",
+    pass5: "div[name='foo'] { .gradient('$*&!*@#()&!#'); }"
+  }
+
+  fail_tests = {
+    fail1: "div[name='foo'] { .gradient($*&!(*@#&!#); }"
+  }
+
+  pass_tests.each do |key, test|
+    should("Satisfy test #{key}") { t test }
+  end
+
+  fail_tests.each do |key, test|
+    should "Fail test #{key}" do
+      assert_raises(::Lesstidy::ParseError) { Lesstidy::Document.new(test) }
+    end
   end
 
 private
